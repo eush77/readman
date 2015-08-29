11f266827452afe02a2ee073c057795e4af9e084
Eugene Sharygin <eush77@gmail.com>
2015-08-29 22:32:12 +0300

    Split main module

--- cli.js
+++ cli.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 'use strict';
 
+var readmeForPackage = require('./lib/readme-for-package');
+
 var help = require('help-version')(usage()).help,
-    readmeFilenames = require('readme-filenames'),
     readmeToManPage = require('readme-to-man-page'),
     manPager = require('man-pager'),
     npmExpansion = require('npm-expansion');
 
-var path = require('path'),
-    fs = require('fs');
+var fs = require('fs');
 
 
 function usage () {
@@ -41,31 +41,6 @@ function usage () {
     });
   }
 }(process.argv.slice(2)));
-
-
-function readmeForPackage (name, cb) {
-  try {
-    require.resolve(name);
-    var root = path.dirname(require.resolve(path.join(name, 'package.json')));
-  }
-  catch (err) {
-    return cb(err);
-  }
-
-  fs.readdir(root, function (err, files) {
-    if (err) return cb(err);
-
-    var readmes = files.filter(function (filename) {
-      return readmeFilenames.indexOf(filename) >= 0;
-    });
-
-    if (readmes.length != 1) {
-      return cb(Error('Readme not found.'));
-    }
-
-    cb(null, path.join(root, readmes[0]));
-  });
-}
 
 
 function error (err) {
